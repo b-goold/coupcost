33a7e70ac743bec9a7cbf87806d53d50643ae117
b-goold <brodiegoold@gmail.com>
2019-03-16 12:36:00 +1100

    Table + JSON Update
    Added some table behaviours, completed half of country JSON

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -9,23 +9,29 @@ var years = [
   { year: '1970s', country: flags[1]}
 ];
 
+var flagMargin = '16px 10px',
+	flagHovMargin = '0px 10px';
+
 d3.select('tbody')
   .selectAll('tr')
   .data(years)
   .enter().append('tr')
   .html(function(d) {
-    return '<th scope="row">' + d.year + '</th>'
-			+ '<img src=' + d.country + ' alt = "">'
+    return '<th scope="row" class="year" width="20%">' + d.year + '</th>'
+			+ '<img class="flag" src=' + d.country + ' alt = "">'
+				+ '<img class="flag" src=' + d.country + ' alt = "">'
   })
 
 d3.select('tbody')
 	.selectAll('img')
+	.style('margin',flagMargin)
 	.on('mouseover', function(d) {
 		d3.select(this)
 		.transition()
 		.duration(300)
 		.style('height','80px')
 		.style('width','80px')
+		.style('margin',flagHovMargin)
 	})
 	.on('mouseout', function(d) {
 		d3.select(this)
@@ -33,4 +39,5 @@ d3.select('tbody')
 		.duration(300)
 		.style('height','64px')
 		.style('width','64px')
+		.style('margin',flagMargin)
 	})
