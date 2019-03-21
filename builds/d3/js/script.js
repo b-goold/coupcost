26f822dc98c3b6e2a366006675c5667ddfa7e617
b-goold <brodiegoold@gmail.com>
2019-03-21 21:47:56 +1100

    Added table population
    Table now populates flags according to decade of coup

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,37 +1,65 @@
-var flags =	[ "https://www.countryflags.io/be/flat/64.png",
-			"https://www.countryflags.io/au/flat/64.png"
-]
-
-<!-- Determine num table rows/columns -->
-
-var years = [
-  { year: '1960s', country: flags[0]},
-  { year: '1970s', country: flags[1]}
-];
-
-var flagMargin = '16px 10px',
-	flagHovMargin = '0px 10px';
-
-d3.select('tbody')
-  .selectAll('tr')
-  .data(years)
-  .enter().append('tr')
-  .html(function(d) {
-    return '<th scope="row" class="year" width="20%">' + d.year + '</th>'
-			+ '<img class="flag" src=' + d.country + ' alt = "">'
-				+ '<img class="flag" src=' + d.country + ' alt = "">'
-  })
-
-d3.select('tbody')
-	.selectAll('img')
-	.style('margin',flagMargin)
+d3.json("js/data/coupdata.json", function(data) {
+	
+	// Iterate through each decade and write info to table
+	// #TODO - Sort by chronological date
+	// #TODO - Add tooltips with a bit more info
+	// #TODO - Add click functionality (last)
+	
+	data.forEach(function (d) {
+		for(var i = 0; i < d.coups.length; i++)
+			if(1959 < d.coups[i].year && d.coups[i].year < 1970) {
+					console.log(d.alpha_country + ", " + d.coups[i].year + ", " + d.coups[i].leader);
+					d3.select('.tr-1960')
+					.append('img')
+					.attr("src", d.flag_url)
+					.attr("class", "flag")
+			}
+			else if(1969 < d.coups[i].year && d.coups[i].year < 1980) {
+					console.log(d.alpha_country + ", " + d.coups[i].year + ", " + d.coups[i].leader);
+					d3.select('.tr-1970')
+					.append('img')
+					.attr("src", d.flag_url)
+					.attr("class", "flag")
+			}
+			else if(1979 < d.coups[i].year && d.coups[i].year < 1990) {
+					console.log(d.alpha_country + ", " + d.coups[i].year + ", " + d.coups[i].leader);
+					d3.select('.tr-1980')
+					.append('img')
+					.attr("src", d.flag_url)
+					.attr("class", "flag")
+			}
+			else if(1989 < d.coups[i].year && d.coups[i].year < 2000) {
+					console.log(d.alpha_country + ", " + d.coups[i].year + ", " + d.coups[i].leader);
+					d3.select('.tr-1990')
+					.append('img')
+					.attr("src", d.flag_url)
+					.attr("class", "flag")
+			}
+			else if(1999 < d.coups[i].year && d.coups[i].year < 2010) {
+					console.log(d.alpha_country + ", " + d.coups[i].year + ", " + d.coups[i].leader);
+					d3.select('.tr-2000')
+					.append('img')
+					.attr("src", d.flag_url)
+					.attr("class", "flag")
+			}
+			else if(2009 < d.coups[i].year && d.coups[i].year < 2020) {
+					console.log(d.alpha_country + ", " + d.coups[i].year + ", " + d.coups[i].leader);
+					d3.select('.tr-2010')
+					.append('img')
+					.attr("src", d.flag_url)
+					.attr("class", "flag")
+			}
+	});
+	
+	// Apply hover functionality
+	
+	d3.selectAll('.flag')
 	.on('mouseover', function(d) {
 		d3.select(this)
 		.transition()
 		.duration(300)
 		.style('height','80px')
 		.style('width','80px')
-		.style('margin',flagHovMargin)
 	})
 	.on('mouseout', function(d) {
 		d3.select(this)
@@ -39,5 +67,6 @@ d3.select('tbody')
 		.duration(300)
 		.style('height','64px')
 		.style('width','64px')
-		.style('margin',flagMargin)
 	})
+	
+});
